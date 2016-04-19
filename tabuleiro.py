9fa3ac287d5f642c80118329d524ca0a8ce92986
mafe2233 <mafe2233@gmai.com>
2016-04-19 16:26:06 -0300

    comecei a fazer o jogo

--- tabuleiro.py
+++ tabuleiro.py
@@ -18,29 +18,41 @@ class Tabuleiro:
         self.janela.rowconfigure(0, minsize = 100)
         self.janela.rowconfigure(1, minsize = 100)
         self.janela.rowconfigure(2, minsize = 100)
+        self.janela.rowconfigure(3, minsize = 100)
         self.janela.columnconfigure(0, minsize = 100)
         self.janela.columnconfigure(1, minsize = 100)
         self.janela.columnconfigure(2, minsize = 100)
+        self.janela.columnconfigure(3, minsize = 100)
         
         self.botao_0_0 = tk.Button(self.janela)
         self.botao_0_0.grid(row=0,column=0,sticky="nsew")
+        
         self.botao_0_1 = tk.Button(self.janela)
         self.botao_0_1.grid(row=0,column=1,sticky="nsew")
+        
         self.botao_0_2 = tk.Button(self.janela)
         self.botao_0_2.grid(row=0,column=2,sticky="nsew")
+        
         self.botao_1_0 = tk.Button(self.janela)
         self.botao_1_0.grid(row=1,column=0,sticky="nsew")
+        
         self.botao_2_0 = tk.Button(self.janela)
         self.botao_2_0.grid(row=2,column=0,sticky="nsew")
+        
         self.botao_1_1 = tk.Button(self.janela)
         self.botao_1_1.grid(row=1,column=1,sticky="nsew")
+        
         self.botao_1_2 = tk.Button(self.janela)
         self.botao_1_2.grid(row=1,column=2,sticky="nsew")
+        
         self.botao_2_1 = tk.Button(self.janela)
         self.botao_2_1.grid(row=2,column=1,sticky="nsew")
+        
         self.botao_2_2 = tk.Button(self.janela)
         self.botao_2_2.grid(row=2,column=2,sticky="nsew")
         
+        self.label = tk.Label(self.janela)
+        self.label.grid(row=3,column=0,columnspan=3,sticky="nsew")
         
         
     def iniciar(self):
