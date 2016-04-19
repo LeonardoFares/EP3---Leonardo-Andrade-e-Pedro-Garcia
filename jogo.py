7ea5ecc4cfb89664c365b48fedb98df31d215374
mafe2233 <mafe2233@gmai.com>
2016-04-19 10:49:41 -0300

    criei o arquivo jogo.py

---
+++ jogo.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+class Jogo:
+    def __init__(self):
+        
+        
+        
+    def recebe_jogada(self,linha,coluna):
+        
+        
+        
+        
+    def verifica_ganhador():
+        
+        
+    def limpa_jogadas():
