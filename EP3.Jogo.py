87a2a67c87c9531e051b7e9ead4d8f5b4f1bf7db
mafe2233 <mafe2233@gmai.com>
2016-04-20 10:41:50 -0300

    criei verifica jogada

--- EP3.Jogo.py
+++ EP3.Jogo.py
@@ -17,7 +17,27 @@ class Jogo:
         if matriz[0][0] and matriz[0][1] and matriz[0][2] == 1:
             print ("jogador 1 ganhou")
             return             
-        
+        elif matriz[0][0] and matriz[1][0] and matriz[2][0] == 1:
+            print ("jogador 1 ganhou")
+            return
+        elif matriz[1][0] and matriz[1][1] and matriz[1][2] == 1:
+            print("jogador 1 ganhou")
+            return
+        elif matriz[2][0] and matriz[2][1] and matriz[2][2] == 1:
+            print("jogador 1 ganhou")
+            return
+        elif matriz[0][0] and matriz[1][1] and matriz[2][2] == 1:
+            print("jogador 1 ganhou")
+            return
+        elif matriz[0][1] and matriz[1][1] and matriz[2][1] == 1:
+            print("jogador 1 ganhou")
+            return
+        elif matriz[0][2] and matriz[1][2] and matriz[2][2] == 1:
+            print("jogador 1 ganhou")
+            return
+        elif matriz[2][0] and matriz[1][1] and matriz[0][2] == 1:
+            print("jogador 1 ganhou")
+            return    
     def limpa_jogadas():
         
         
