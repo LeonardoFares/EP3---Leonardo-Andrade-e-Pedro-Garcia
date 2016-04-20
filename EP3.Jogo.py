4a250c4cd670ace5494c14f09a2bc08bd1bde674
mafe2233 <mafe2233@gmai.com>
2016-04-20 10:29:40 -0300

    arrumei os arquivos

---
+++ EP3.Jogo.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+class Jogo:
+    def __init__(self):
+        self.jogador = 1
+        self.matriz = np.zeros([3,3])                
+        
+        
+    def recebe_jogada(self,linha,coluna):
+        self.matriz[linha][coluna]=self.jogador     
+        if self.jogador == 1:
+            self.jogador = 2
+        else:
+            self.jogador = 1
+                
+    def verifica_ganhador():
+        if matriz[0][0] and matriz[0][1] and matriz[0][2] == 1:
+            print ("jogador 1 ganhou")
+            return             
+        
+    def limpa_jogadas():
+        
+        
+        
