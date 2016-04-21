a7ec44a8740788272080240d3f6bf44d9d088b0d
LeonardoFares <leofcandrade@gmail.com>
2016-04-21 12:55:09 -0300

    Ajustei a função verifica_ganhador

--- EP3.Jogo.py
+++ EP3.Jogo.py
@@ -14,22 +14,41 @@ class Jogo:
             self.jogador = 1
                 
     def verifica_ganhador(self):
-        if self.matriz[0][0] and self.matriz[0][1] and self.matriz[0][2] == self.jogador:
-            return             
-        elif self.matriz[0][0] and self.matriz[1][0] and self.matriz[2][0] == 1:
-            return
-        elif self.matriz[1][0] and self.matriz[1][1] and self.matriz[1][2] == 1:
-            return
-        elif self.matriz[2][0] and self.matriz[2][1] and self.matriz[2][2] == 1:
-            return
-        elif self.matriz[0][0] and self.matriz[1][1] and self.matriz[2][2] == 1:
-            return
-        elif self.matriz[0][1] and self.matriz[1][1] and self.matriz[2][1] == 1:
-            return
-        elif self.matriz[0][2] and self.matriz[1][2] and self.matriz[2][2] == 1:
-            return
-        elif self.matriz[2][0] and self.matriz[1][1] and self.matriz[0][2] == 1:
-            return 1   
+        if self.matriz[0][0] == 1 and self.matriz[0][1] == 1 and self.matriz[0][2] == 1:
+            return 1            
+        elif self.matriz[0][0] == 1 and self.matriz[1][0] == 1 and self.matriz[2][0] == 1:
+            return 1
+        elif self.matriz[1][0] == 1 and self.matriz[1][1] == 1 and self.matriz[1][2] == 1:
+            return 1
+        elif self.matriz[2][0] == 1 and self.matriz[2][1] == 1 and self.matriz[2][2] == 1:
+            return 1
+        elif self.matriz[0][0] == 1 and self.matriz[1][1] == 1 and self.matriz[2][2] == 1:
+            return 1
+        elif self.matriz[0][1] == 1 and self.matriz[1][1] == 1 and self.matriz[2][1] == 1:
+            return 1
+        elif self.matriz[0][2] == 1 and self.matriz[1][2] == 1 and self.matriz[2][2] == 1:
+            return 1
+        elif self.matriz[2][0] == 1 and self.matriz[1][1] == 1 and self.matriz[0][2] == 1:
+            return 1
+        elif self.matriz[0][0] == 2 and self.matriz[0][1] == 2 and self.matriz[0][2] == 2:
+            return 2            
+        elif self.matriz[0][0] == 2 and self.matriz[1][0] == 2 and self.matriz[2][0] == 2:
+            return 2
+        elif self.matriz[1][0] == 2 and self.matriz[1][1] == 2 and self.matriz[1][2] == 2:
+            return 2
+        elif self.matriz[2][0] == 2 and self.matriz[2][1] == 2 and self.matriz[2][2] == 2:
+            return 2
+        elif self.matriz[0][0] == 2 and self.matriz[1][1] == 2 and self.matriz[2][2] == 2:
+            return 2
+        elif self.matriz[0][1] == 2 and self.matriz[1][1] == 2 and self.matriz[2][1] == 2:
+            return 2
+        elif self.matriz[0][2] == 2 and self.matriz[1][2] == 2 and self.matriz[2][2] == 2:
+            return 2
+        elif self.matriz[2][0] == 2 and self.matriz[1][1] == 2 and self.matriz[0][2] == 2:
+            return 2  
+        elif self.matriz 
+            return 0
+            
     def limpa_jogadas(self):
         self.matriz = np.zeros([3,3])
 
