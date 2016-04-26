e7affb90d0ad25293632595ca03e947843f2ae69
mafe2233 <mafe2233@gmai.com>
2016-04-26 11:35:17 -0300

    arrumei os botoes(mais ou menos)

--- tabuleiro.py
+++ tabuleiro.py
@@ -27,8 +27,8 @@ class Tabuleiro:
         self.janela.columnconfigure(3, minsize = 100)
         
         self.label = tk.Label(self.janela)
-        self.label.configure(text="Proximo jogador")
-        self.label.grid(row=3, column=0, columnspan=2, sticky="nsew")         
+        self.label.configure(text="Proximo jogador:1")
+        self.label.grid(row=3, column=0, columnspan=3, sticky="nsew")         
         
         self.botao = [[1,2,3],
                        [4,5,6],
@@ -70,27 +70,50 @@ class Tabuleiro:
         self.botao[2][2] = tk.Button(self.janela)
         self.botao[2][2].grid(row=2,column=2,sticky="nsew")
         self.botao[2][2].configure(command=self.clicar_botao22)
+    
+    
+    def reiniciar(self):
+        self.Comandos.limpa_jogadas()
+        self.botao[0][0].config(text="")        
+        self.botao[0][0].config(state="normal")
+        self.botao[0][1].config(text="")        
+        self.botao[0][1].config(state="normal")
+        self.botao[0][2].config(text="")        
+        self.botao[0][2].config(state="normal")
+        self.botao[1][0].config(text="")        
+        self.botao[1][0].config(state="normal")
+        self.botao[2][0].config(text="")        
+        self.botao[2][0].config(state="normal")
+        self.botao[1][1].config(text="")        
+        self.botao[1][1].config(state="normal")
+        self.botao[2][1].config(text="")        
+        self.botao[2][1].config(state="normal")
+        self.botao[1][2].config(text="")        
+        self.botao[1][2].config(state="normal")
+        self.botao[2][2].config(text="")        
+        self.botao[2][2].config(state="normal")        
         
-        self.label = tk.Label(self.janela)
-        self.label.grid(row=3,column=0,columnspan=2,sticky="nsew")
+        
  
-        self.botao_reinicio = tk.Button(self.janela)
-        self.botao_reinicio.grid(row=3,column=3,columnspan = 1, sticky="nsew")
-        self.botao_reinicio.configure(command=self.Comandos.limpa_jogadas())
-        
-       
-        
+    def botao_reinicio(self):
+         self.reinicio = tk.Button(self.janela)
+         self.reinicio.grid(row=3,column=2,sticky="nsew")
+         self.reinicio.configure(text="reiniciar")
+         self.reinicio.configure(command=self.reiniciar())       
+         
        
     def vitoria(self):
         resultado = self.Comandos.verifica_ganhador()
         print (resultado)
         if resultado == 1:
             self.label.configure(text="Jogador 1 ganhou!")
-            self.reinicio
+            self.botao_reinicio()
         elif resultado == 2:
             self.label.configure(text="Jogador 2 ganhou")
+            self.botao_reinicio()
         elif resultado == 0:
             self.label.configure(text="Empate!")
+            self.botao_reinicio()
         else:
             self.label.configure(text="Proximo jogador: {0}".format(self.Comandos.jogador))
         
@@ -102,12 +125,14 @@ class Tabuleiro:
             self.botao[0][0].config(text="o")
         self.Comandos.recebe_jogada(0,0)
         self.vitoria()
+        self.botao[0][0].config(state="disable")
     def clicar_botao01(self):        
         if self.Comandos.jogador == 1:
             self.botao[0][1].config(text="x")
         else:
             self.botao[0][1].config(text="o")
         self.Comandos.recebe_jogada(0,1)    
+        self.botao[0][1].config(state="disable")
         self.vitoria()
     def clicar_botao02(self):
         if self.Comandos.jogador == 1:
@@ -116,6 +141,7 @@ class Tabuleiro:
             self.botao[0][2].config(text="o")    
         self.Comandos.recebe_jogada(0,2)
         self.vitoria()
+        self.botao[0][2].config(state="disable")
     def clicar_botao10(self):
         if self.Comandos.jogador == 1:
             self.botao[1][0].config(text="x")
@@ -123,6 +149,7 @@ class Tabuleiro:
             self.botao[1][0].config(text="o")    
         self.Comandos.recebe_jogada(1,0)
         self.vitoria()
+        self.botao[1][0].config(state="disable")
     def clicar_botao20(self):
         if self.Comandos.jogador == 1:
             self.botao[2][0].config(text="x")
@@ -130,6 +157,7 @@ class Tabuleiro:
             self.botao[2][0].config(text="o")    
         self.Comandos.recebe_jogada(2,0)
         self.vitoria()
+        self.botao[2][0].config(state="disable")
     def clicar_botao11(self):
         if self.Comandos.jogador == 1:
             self.botao[1][1].config(text="x")
@@ -137,6 +165,7 @@ class Tabuleiro:
             self.botao[1][1].config(text="o")    
         self.Comandos.recebe_jogada(1,1)
         self.vitoria() 
+        self.botao[1][1].config(state="disable")
     def clicar_botao12(self):
         if self.Comandos.jogador == 1:
             self.botao[1][2].config(text="x")
@@ -144,6 +173,7 @@ class Tabuleiro:
             self.botao[1][2].config(text="o")    
         self.Comandos.recebe_jogada(1,2)
         self.vitoria() 
+        self.botao[1][2].config(state="disable")
     def clicar_botao21(self):
         if self.Comandos.jogador == 1:
             self.botao[2][1].config(text="x")
@@ -151,6 +181,7 @@ class Tabuleiro:
             self.botao[2][1].config(text="o")    
         self.Comandos.recebe_jogada(2,1)
         self.vitoria() 
+        self.botao[2][1].config(state="disable")
     def clicar_botao22(self):
         if self.Comandos.jogador == 1:
             self.botao[2][2].config(text="x")
@@ -158,6 +189,7 @@ class Tabuleiro:
             self.botao[2][2].config(text="o")    
         self.Comandos.recebe_jogada(2,2)
         self.vitoria()             
+        self.botao[2][2].config(state="disable")
      
         
      
